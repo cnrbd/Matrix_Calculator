f51e829d337b5fc0524ad76fc0fa6ad787ae62fa
cnrbd <puong23@ssis.edu.vn>
2024-02-21 21:00:07 -0500

    changes

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def determinant_calculation(matrix):
     matrix = np.array(array_list).reshape(rows,columns)
     return (det(matrix))
 
-# function that transpose a matrix
+# function that transpose a matrix using numpy
 def transpose_calculation(matrix):
     rows = 3 
     columns = 3 
